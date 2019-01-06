183ed3f2a565df67380b011a340044e214fc9218
dimxy <dimxy@komodoplatform.com>
2019-01-06 21:14:05 +0500

    token & asset validation turned on

--- src/cc/CCtokens.cpp
+++ src/cc/CCtokens.cpp
@@ -148,7 +148,7 @@ bool TokensValidate(struct CCcontract_info *cp, Eval* eval, const CTransaction &
 	char destaddr[64], origaddr[64], CCaddr[64];
 	std::vector<CPubKey> voutTokenPubkeys;
 
-	return true;
+	//return true;
 
 	numvins = tx.vin.size();
 	numvouts = tx.vout.size();
