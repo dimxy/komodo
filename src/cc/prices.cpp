1d4f7c3ea57d289b8d1239fccdefb88ab1529f25
dimxy <dimxy@komodoplatform.com>
2019-05-10 12:47:25 +0500

    corr height

--- src/cc/prices.cpp
+++ src/cc/prices.cpp
@@ -1643,7 +1643,7 @@ int32_t prices_getbetinfo(uint256 bettxid, BetInfo &betinfo)
 
         // TODO: forget old tx
         CBlockIndex *bi = komodo_getblockindex(hashBlock);
-        if (bi && bi->GetHeight() > 5342)
+        if (bi && bi->GetHeight() < 5342)
             return -5;
 
         OneBetData bet1;
@@ -2021,7 +2021,7 @@ UniValue PricesList(uint32_t filter, CPubKey mypk)
 
             // TODO: forget old tx
             CBlockIndex *bi = komodo_getblockindex(hashBlock);
-            if (bi && bi->GetHeight() > 5342)
+            if (bi && bi->GetHeight() < 5342)
                 return;
 
             bool bAppend = false;
