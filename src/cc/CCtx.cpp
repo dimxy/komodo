c3afed40156533af3912fd6fc0efd938d6e2a7e0
dimxy <dimxy@komodoplatform.com>
2019-05-31 15:00:26 +0500

    try cc_conditionBinary

--- src/cc/CCtx.cpp
+++ src/cc/CCtx.cpp
@@ -285,10 +285,6 @@ std::string FinalizeCCTx(uint64_t CCmask,struct CCcontract_info *cp,CMutableTran
                     }
                 }
 
-                std::cerr << "CCtx before test CCSig" << std::endl;
-                CCSig(cond);
-                std::cerr << "CCtx after test CCSig" << std::endl;
-
                 uint256 sighash = SignatureHash(CCPubKey(cond), mtx, i, SIGHASH_ALL, utxovalues[i],consensusBranchId, &txdata);
                 if ( cc_signTreeSecp256k1Msg32(cond,privkey,sighash.begin()) != 0 )
                 {
