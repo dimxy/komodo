47ee62712cc659d5d067743f6bf8be2701e5c71e
dimxy <dimxy@komodoplatform.com>
2019-07-29 23:20:06 +0500

    corr rpc param num check

--- src/rpc/ccrpc.cpp
+++ src/rpc/ccrpc.cpp
@@ -566,7 +566,7 @@ UniValue kogsaddkogstocontainer(const UniValue& params, bool fHelp)
     if (error < 0)
         throw runtime_error(strprintf("to use CC contracts, you need to launch daemon with valid -pubkey= for an address in your wallet. ERR=%d\n", error));
 
-    if (fHelp || (params.size() != 2))
+    if (fHelp || (params.size() < 2))
     {
         throw runtime_error(
             "kogsaddkogstocontainer containerid tokenid1, tokenid2, ...\n"
@@ -613,7 +613,7 @@ UniValue kogsremovekogsfromcontainer(const UniValue& params, bool fHelp)
     if (error < 0)
         throw runtime_error(strprintf("to use CC contracts, you need to launch daemon with valid -pubkey= for an address in your wallet. ERR=%d\n", error));
 
-    if (fHelp || (params.size() != 2))
+    if (fHelp || (params.size() != 1))
     {
         throw runtime_error(
             "kogsremovekogsfromcontainer '{ \"containerid\":\"id\", \"gameid\":\"id\", \"tokenids\" : [tokenid1, tokenid2, ...] }'\n"
