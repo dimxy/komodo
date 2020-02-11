a24c81dd1ab063cc9debc83686b984619fc86350
dimxy <dimxy@komodoplatform.com>
2020-02-12 02:44:40 +0500

    skip txfee check

--- src/cc/marmara.cpp
+++ src/cc/marmara.cpp
@@ -1949,7 +1949,7 @@ bool MarmaraValidate(struct CCcontract_info *cp, Eval* eval, const CTransaction 
     CAmount txfee;
     const CAmount max_txfee = 10000;
     txfee = get_txfee(tx);
-    if (txfee < 0 || txfee > max_txfee)
+    if ((txfee < 0 || txfee > max_txfee) && chainActive.LastTip() && chainActive.LastTip()->GetHeight() >= MARMARA_POS_IMPROVEMENTS_HEIGHT)
     {
         LOGSTREAMFN("marmara", CCLOG_ERROR, stream << " validation error '" << "bad txfee=" << txfee << "' for tx=" << HexStr(E_MARSHAL(ss << tx)) << std::endl);
         return eval->Error("incorrect txfee");
