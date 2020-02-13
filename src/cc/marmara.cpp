227a4bb33143671ab7a2323df6c46d030dbb4a03
dimxy <dimxy@komodoplatform.com>
2020-02-13 23:35:42 +0500

    include

--- src/cc/marmara.cpp
+++ src/cc/marmara.cpp
@@ -14,6 +14,7 @@
  *                                                                            *
  ******************************************************************************/
 
+#include <stdlib.h>
 #include <list>
 #include "komodo_defs.h"
 #include "CCMarmara.h"
