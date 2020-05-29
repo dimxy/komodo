a2325103fb46a4acdb9047c6d801923ae00e3f39
dimxy <dimxy@komodoplatform.com>
2020-05-30 00:15:44 +0500

    comment added

--- src/cc/CCMarmara.h
+++ src/cc/CCMarmara.h
@@ -53,9 +53,10 @@ enum MARMARA_FUNCID : uint8_t {
     MARMARA_POOL = 'P'
 };
 
-const int32_t MARMARA_POS_IMPROVEMENTS_HEIGHT = 110777;
-const uint32_t MARMARA_2020JUNE_UPDATE_TIMESTAMP = nS4Timestamp;
-
+// Marmara consensus update variables:
+const int32_t MARMARA_POS_IMPROVEMENTS_HEIGHT = 110777;  // first update with fixing consensus issues on March 2020
+const uint32_t MARMARA_2020JUNE_UPDATE_TIMESTAMP = nS4Timestamp;  // second update (marmaraunlock) synced with annual NN Season 4 on Jun 2020
+ 
 // return true if new version activation time is passed
 inline static bool MarmaraIs2020JuneUpdateActive(const Eval *eval)
 {
