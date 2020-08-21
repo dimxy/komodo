c6ded129cacb510c6445f2a00304a7eaefa1284b
dimxy <dimxy@komodoplatform.com>
2020-08-21 19:47:41 +0300

    pytests updated from komodo_ci

--- qa/pytest_komodo/ci_setup.sh
+++ qa/pytest_komodo/ci_setup.sh
@@ -10,7 +10,7 @@ export TEST_ADDY1="RHoTHYiHD8TU4k9rbY4Aoj3ztxUARMJikH"
 export TEST_WIF1="UwmmwgfXwZ673brawUarPzbtiqjsCPWnG311ZRAL4iUCZLBLYeDu"
 export TEST_PUBKEY1="0285f68aec0e2f8b5e817d71a2a20a1fda74ea9943c752a13136a3a30fa49c0149"
 export CHAIN_MODE="REGULAR"
-export IS_BOOTSTRAP_NEEDED="True"
+export IS_BOOTSTRAP_NEEDED="False"
 export BOOTSTRAP_URL="https://sirseven.me/share/bootstrap.tar.gz"
 
 # starting the chains
