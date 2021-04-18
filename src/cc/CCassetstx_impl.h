df1630e50b553c821ccae738b9f335595abbcbee
dimxy <dimxy@komodoplatform.com>
2021-04-18 12:41:59 +0500

    added check if spending tx in active chain for assets rpc

--- src/cc/CCassetstx_impl.h
+++ src/cc/CCassetstx_impl.h
@@ -20,6 +20,25 @@
 #include "CCassets.h"
 #include "CCNFTData.h"
 
+
+static bool IsTxidInActiveChain(uint256 txid)
+{
+    CTransaction tx;
+    uint256 hashBlock;
+    AssertLockHeld(cs_main);
+
+    if (myGetTransaction(txid, tx, hashBlock))
+    {
+        BlockMap::iterator mi = mapBlockIndex.find(hashBlock);
+        if (mi != mapBlockIndex.end() && (*mi).second) {
+            CBlockIndex* pindex = (*mi).second;
+            if (chainActive.Contains(pindex)) 
+                return true;
+        }
+    }
+    return false;
+}
+
 template<class T, class A>
 UniValue AssetOrders(uint256 refassetid, CPubKey pk, uint8_t evalcodeNFT)
 {
@@ -376,7 +395,8 @@ UniValue CancelBuyOffer(const CPubKey &mypk, int64_t txfee,uint256 assetid,uint2
         int32_t spendingvin, h;
 
         mask = ~((1LL << mtx.vin.size()) - 1);
-        if (CCgetspenttxid(spendingtxid, spendingvin, h, bidtxid, ASSETS_GLOBALADDR_VOUT) != 0 && myGetTransaction(bidtxid, vintx, hashBlock) && vintx.vout.size() > ASSETS_GLOBALADDR_VOUT)
+        LOCK(cs_main);
+        if ((CCgetspenttxid(spendingtxid, spendingvin, h, bidtxid, ASSETS_GLOBALADDR_VOUT) != 0 || !IsTxidInActiveChain(spendingtxid))  && myGetTransaction(bidtxid, vintx, hashBlock) && vintx.vout.size() > ASSETS_GLOBALADDR_VOUT)
         {
             uint8_t dummyEvalCode; uint256 dummyAssetid, dummyAssetid2; int64_t dummyPrice; std::vector<uint8_t> dummyOrigpubkey;
 
@@ -439,7 +459,8 @@ UniValue CancelSell(const CPubKey &mypk, int64_t txfee, uint256 assetid, uint256
         int32_t spendingvin, h;
 
         mask = ~((1LL << mtx.vin.size()) - 1);
-        if (CCgetspenttxid(spendingtxid, spendingvin, h, asktxid, ASSETS_GLOBALADDR_VOUT) != 0 && myGetTransaction(asktxid, vintx, hashBlock) != 0 && vintx.vout.size() > 0)
+        LOCK(cs_main);
+        if ((CCgetspenttxid(spendingtxid, spendingvin, h, asktxid, ASSETS_GLOBALADDR_VOUT) != 0 || !IsTxidInActiveChain(spendingtxid)) && myGetTransaction(asktxid, vintx, hashBlock) != 0 && vintx.vout.size() > 0)
         {
             uint8_t dummyEvalCode; 
             uint256 dummyAssetid, dummyAssetid2; 
@@ -543,7 +564,8 @@ UniValue FillBuyOffer(const CPubKey &mypk, int64_t txfee, uint256 assetid, uint2
         int32_t spendingvin, h;
 
         mask = ~((1LL << mtx.vin.size()) - 1);
-        if (CCgetspenttxid(spendingtxid, spendingvin, h, bidtxid, bidvout) != 0 && myGetTransaction(bidtxid, vintx, hashBlock) != 0 && vintx.vout.size() > bidvout)
+        LOCK(cs_main);
+        if ((CCgetspenttxid(spendingtxid, spendingvin, h, bidtxid, bidvout) != 0 || !IsTxidInActiveChain(spendingtxid)) && myGetTransaction(bidtxid, vintx, hashBlock) != 0 && vintx.vout.size() > bidvout)
         {
             bid_amount = vintx.vout[bidvout].nValue;
             uint8_t funcid = SetAssetOrigpubkey<A>(origpubkey, unit_price, vintx);  // get orig pk, orig units
@@ -625,7 +647,6 @@ UniValue FillBuyOffer(const CPubKey &mypk, int64_t txfee, uint256 assetid, uint2
     return "";
 }
 
-
 // send coins, receive tokens 
 template<class T, class A>
 UniValue FillSell(const CPubKey &mypk, int64_t txfee, uint256 assetid, uint256 assetid2, uint256 asktxid, int64_t fillunits, CAmount paid_unit_price)
@@ -671,7 +692,8 @@ UniValue FillSell(const CPubKey &mypk, int64_t txfee, uint256 assetid, uint256 a
     uint256 spendingtxid;
     int32_t spendingvin, h;
 
-    if (CCgetspenttxid(spendingtxid, spendingvin, h, asktxid, askvout) != 0 && myGetTransaction(asktxid, vintx, hashBlock) != 0 && vintx.vout.size() > askvout)
+    LOCK(cs_main);
+    if ((CCgetspenttxid(spendingtxid, spendingvin, h, asktxid, askvout) != 0 || !IsTxidInActiveChain(spendingtxid)) && myGetTransaction(asktxid, vintx, hashBlock) && vintx.vout.size() > askvout)
     {
         orig_assetoshis = vintx.vout[askvout].nValue;
         uint8_t funcid = SetAssetOrigpubkey<A>(origpubkey, unit_price, vintx); // get orig pk, orig value
