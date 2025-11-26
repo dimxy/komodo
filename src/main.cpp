845d5dc215b0ed19c5e0dd2c89c5c4ca0fef32ed
dimxy <dimxy@komodoplatform.com>
2025-11-26 23:16:20 +0500

    remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -8050,12 +8050,6 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                 Misbehaving(pfrom->GetId(), 20);
                 return error("non-continuous headers sequence");
             }
-
-            int haltAt = GetArg("-haltat", 0);
-            if (pindexLast && pindexLast->nHeight >= haltAt) {
-                return error("halt headers");
-            }
-
             int32_t futureblock;
             if (!AcceptBlockHeader(&futureblock,header, state, &pindexLast)) {
                 int nDoS;
