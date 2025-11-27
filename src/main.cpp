a1e8c4cd817e0c6d717e4eacccf2ef0fb53522de
dimxy <dimxy@komodoplatform.com>
2025-11-27 19:55:22 +0500

    remove some logging

--- src/main.cpp
+++ src/main.cpp
@@ -8441,7 +8441,7 @@ bool SendMessages(CNode* pto, bool fSendTrickle)
         if (pto->nVersion == 0)
             return true;
 
-        LogPrintf("SendMessages for node %d.. enterred\n", pto->id);
+        //LogPrintf("SendMessages for node %d.. enterred\n", pto->id); //TODO test
         //
         // Message: ping
         //
@@ -8651,13 +8651,13 @@ bool SendMessages(CNode* pto, bool fSendTrickle)
         //
         static uint256 zero;
         vector<CInv> vGetData;
-        LogPrintf("SendMessages about to download blocks pto->fClient=%d fFetch=%d IsInitialBlockDownload()=%d state.nBlocksInFlight=%d\n", 
+        //LogPrintf("SendMessages about to download blocks pto->fClient=%d fFetch=%d IsInitialBlockDownload()=%d state.nBlocksInFlight=%d\n", 
             pto->fClient, fFetch, IsInitialBlockDownload(), state.nBlocksInFlight);
         if (!pto->fDisconnect && !pto->fClient && (fFetch || !IsInitialBlockDownload()) && state.nBlocksInFlight < MAX_BLOCKS_IN_TRANSIT_PER_PEER) {
             vector<CBlockIndex*> vToDownload;
             NodeId staller = -1;
             FindNextBlocksToDownload(pto->GetId(), MAX_BLOCKS_IN_TRANSIT_PER_PEER - state.nBlocksInFlight, vToDownload, staller);
-            LogPrintf("SendMessages vToDownload.size()=%d\n", vToDownload.size());
+            //LogPrintf("SendMessages vToDownload.size()=%d\n", vToDownload.size());
             BOOST_FOREACH(CBlockIndex *pindex, vToDownload) {
                 vGetData.push_back(CInv(MSG_BLOCK, pindex->GetBlockHash()));
                 MarkBlockAsInFlight(pto->GetId(), pindex->GetBlockHash(), consensusParams, pindex);
