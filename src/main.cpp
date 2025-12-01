fec0cc16117ccbfe12aa8e60edd4d38f8596bec9
dimxy <dimxy@komodoplatform.com>
2025-12-01 21:48:05 +0500

    fix new blk activ ht

--- src/main.cpp
+++ src/main.cpp
@@ -5832,7 +5832,8 @@ bool ProcessNewBlock(bool from_miner, int32_t height, CValidationState &state, C
         return error("%s: ActivateBestChain failed", __func__);
 
     Checkpoints::SyncChkParams syncChkParams;
-    LogPrintf("%s calling IsSyncCheckpointUpgradeActive height=%d pblock->GetBlockTime()=%d\n", __func__, height, pblock->GetBlockTime());
+    int nHeightActiv = height != 0 ? height : komodo_block2height(pblock);
+    LogPrintf("%s calling IsSyncCheckpointUpgradeActive nHeightActiv=%d pblock->GetBlockTime()=%d\n", __func__, nHeightActiv, pblock->GetBlockTime());
     if (Checkpoints::IsSyncCheckpointUpgradeActive(syncChkParams, height, pblock->GetBlockTime())) {
         if (!TryInitSyncCheckpoint(syncChkParams))
             return error("%s() : failed to initialize sync checkpoint", __func__);  
