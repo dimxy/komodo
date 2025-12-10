6fd88b3fa5ef2bab897a040be76369559586eb37
dimxy <dimxydev@yahoo.com>
2025-12-10 15:32:11 +0500

    add license

--- src/Gulden/auto_checkpoints.cpp
+++ src/Gulden/auto_checkpoints.cpp
@@ -3,6 +3,21 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+/******************************************************************************
+ * Copyright © 2025 The SuperNET Developers.                                  *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * SuperNET software, including this file may be copied, modified, propagated *
+ * or distributed except according to the terms contained in the LICENSE file *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
 #include "checkpoints.h"
 #include "auto_checkpoints.h"
 #include "consensus/validation.h"
