601d7318222ac99254d9362125a92da5b193f1f0
dimxy <dimxydev@yahoo.com>
2025-12-13 13:31:31 +0500

    set test chk activ time

--- src/Gulden/auto_checkpoints_activation.cpp
+++ src/Gulden/auto_checkpoints_activation.cpp
@@ -37,30 +37,11 @@ namespace Checkpoints
 
         // TODO: fix master key
         CSyncCheckpointActivation() {
-            mainnet_params = CSyncChkParams { nSyncChkPointHeight, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" };
+            mainnet_params = boost::none;
             testnet_params = boost::none;
 
             asset_chains = {
-                { "CCL", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "CLC", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "GLEEC", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "ILN", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "KOIN", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "PIRATE", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "THC", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "BCZERO", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "RAPH", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "MDX", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-
-
-                // test chains:
-                { "DOC", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-                { "MARTY", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }},
-
-                // test chain
-                // auto checkpoint active since Dec, 01 2025
-                { "GULDEN", { 1764606619, "02f9dc5271cc789aab77fb27e8007e681f93135cfcf92d4a514a4649c0e36f14ad" }}
-                // TODO: add asset chains
+                { "MCL", { nSyncChkPointTimestamp, "03fdc6ca526c0cfaed2211d03dc2ea9c083aea127c7769d97dc92fed2085803ce3" }}
             };
         }
 
--- src/komodo_defs.h
+++ src/komodo_defs.h
@@ -95,11 +95,15 @@ static const int32_t KMD_SEASON_HEIGHTS[NUM_KMD_SEASONS] = {
     nS8HardforkHeight,
     8113400};
 
-const int32_t nSunsettingHeight = 5247009; // TODO fix (now approx Dec 01 2026)
-const uint32_t nSunsettingTimestamp = 1796103821; // TODO fix, now Dec 01 2026
+// KMD mainnet height sample
+const int32_t nSunsettingHeight = 4722174;
+// Asset chain timestamp
+const uint32_t nSunsettingTimestamp = 1764606619; // TODO fix, (now 1 Dec 2025)
 
-const int32_t nSyncChkPointHeight = nSunsettingHeight; // TODO fix
-const uint32_t nSyncChkPointTimestamp = nSunsettingTimestamp; // TODO fix
+// KMD mainnet height
+const int32_t nSyncChkPointHeight = nSunsettingHeight; // TODO fix if used
+// Asset chain timestamp
+const uint32_t nSyncChkPointTimestamp = nSunsettingTimestamp; // TODO fix if used
 
 // Era array of pubkeys. Add extra seasons to bottom as requried, after adding appropriate info above. 
 static const char *notaries_elected[NUM_KMD_SEASONS][NUM_KMD_NOTARIES][2] =
